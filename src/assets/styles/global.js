b06a99ee682d23000c00ea0e7248b1f1384e5eba
Alan <allanrds26@gmail.com>
2023-02-10 14:18:54 -0300

    Global Style

---
+++ src/assets/styles/global.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+
+  :root{
+    --laranja: #FBAB4D;
+    
+  }
+
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+ }
+
+
+
+
+`
