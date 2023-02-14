a469f0c904394a6efc35a6d338ee4c03a84bacfa
Alan Silva <107173261+alandoscriativos@users.noreply.github.com>
2023-02-14 11:15:52 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+Import RemoverPf from './RemoverPf';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <RemoverPf/>
+
     <App />
   </React.StrictMode>
 );
